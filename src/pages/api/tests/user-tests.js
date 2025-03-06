271be7649518e86bf2f5e86d3e070b57ad4b8617
2pasha <kostishinpavlo24@gmail.com>
2025-03-06 11:15:46 +0200

    feat: add API endpoint for fetching tests based on user role

---
+++ src/pages/api/tests/user-tests.js
@@ -0,0 +1,35 @@
+import authMiddleware from "@/utils/authMiddleware";
+import { Test, TestResult } from "@/utils/db";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  authMiddleware(req, res, async () => {
+    try {
+      const { id, role } = req.user;
+
+      if (role === "teacher") {
+        const tests = await Test.findAll({ where: { teacherId: id } });
+
+        return res.json({ tests });
+      }
+
+      if (role === "student") {
+        const completedTests = await TestResult.findAll({
+          where: { studentId: id },
+          include: [{ model: Test }],
+        });
+
+        return res.json({ tests: completedTests.map((result) => result.Test) });
+      }
+
+      res.status(403).json({ message: "Invalid role" });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Error fetching tests", error: error.message });
+    }
+  });
+}
