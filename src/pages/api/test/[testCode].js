746b111239fd968170a386a1d638b61d70a73f91
2pasha <kostishinpavlo24@gmail.com>
2025-03-11 11:44:59 +0200

    feat: enhance test API response to include test details and description

--- src/pages/api/test/[testCode].js
+++ src/pages/api/test/[testCode].js
@@ -1,5 +1,5 @@
 import authMiddleware from "@/utils/authMiddleware";
-import { Question, GeneratedTest } from "@/utils/db";
+import { Question, GeneratedTest, Test } from "@/utils/db";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
@@ -18,11 +18,22 @@ export default async function handler(req, res) {
       return res.status(404).json({ message: "Test not found" });
     }
 
+    const testDetails = await Test.findByPk(generatedTest.testId);
+    if (!testDetails) {
+      return res.status(404).json({ message: "Test details not found" });
+    }
+
     const questions = await Question.findAll({
       where: { id: generatedTest.questionIds },
     });
 
-    res.status(200).json({ testCode, questions });
+    res.status(200).json({
+      testId: testDetails.id,
+      testCode,
+      testName: testDetails.testName,
+      testDescription: testDetails.testDescription,
+      questions,
+    });
   } catch (error) {
     res
       .status(500)
